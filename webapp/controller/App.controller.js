4dbaef24fbc1e81fddc250feb3314c66d783b637
Srijan Sarmah <68153952+srijan9864@users.noreply.github.com>
2021-05-28 13:19:37 +0530

    Added proxy
    Added proxy for CORS

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -16,63 +16,104 @@ sap.ui.define([
 			});
 
 			Device.orientation.attachHandler(this.onOrientationChange, this);
+			function getDate(n) {
+				var date = new Date();
+				var today = new Date(date);
+				today.setDate(today.getDate() + n);
+				var dd = today.getDate();
+				var mm = today.getMonth() + 1;
+				var yyyy = today.getFullYear();
+				if (dd < 10) {
+					dd = '0' + dd;
+				}
+				if (mm < 10) {
+					mm = '0' + mm;
+				}
+				var today = dd + '-' + mm + '-' + yyyy;
+
+				return today;
+			}
+			var oDate = {
+				dates: {
+					today: getDate(0),
+					today_1: getDate(1),
+					today_2: getDate(2),
+					today_3: getDate(3),
+					today_4: getDate(4),
+					today_5: getDate(5),
+					today_6: getDate(6)
+				}
+			};
+			var oDateModel = new sap.ui.model.json.JSONModel(oDate);
+			this.getView().setModel(oDateModel, 'dates');
+
+			var oModel1 = new sap.ui.model.json.JSONModel();
+			var oSettingsModel = new sap.ui.model.json.JSONModel({ navigatedItem: "" });
+			this.getView().setModel(oModel1);
+			this.getView().setModel(oSettingsModel, 'settings');
 			//      1.Get the id of the VizFrame
-		var oVizFrame = this.getView().byId("idpiechart");
+			var oVizFrame = this.getView().byId("idpiechart");
 
-		//      2.Create a JSON Model and set the data
-				var oModel = new sap.ui.model.json.JSONModel();
-				var data = {
-					'Vaccine' : [{
-						  "Vaccination": "Dose 1",
-						  "Count": "156842545"
-						}, {
-						  "Vaccination": "Dose 2",
-						  "Count": "42281658"
-						}, {
-						  "Vaccination": "Not Vaccinated",
-						  "Count": "1153518077"
-						}]};
-				oModel.setData(data);
+			//      2.Create a JSON Model and set the data
+			var oModel = new sap.ui.model.json.JSONModel();
+			var data = {
+				'Vaccine': [{
+					"Vaccination": "Dose 1",
+					"Count": "156842545"
+				}, {
+					"Vaccination": "Dose 2",
+					"Count": "42281658"
+				}, {
+					"Vaccination": "Not Vaccinated",
+					"Count": "1153518077"
+				}]
+			};
+			oModel.setData(data);
+			console.log(oModel);
 
-		//      3. Create Viz dataset to feed to the data to the graph
-				var oDataset = new sap.viz.ui5.data.FlattenedDataset({
-					dimensions : [{
-							name : 'Vaccination',
-						value : "{Vaccination}"}],
+			   //  3. Create Viz dataset to feed to the data to the graph
+			var oDataset = new sap.viz.ui5.data.FlattenedDataset({
+				dimensions: [{
+					name: 'Vaccination',
+					value: "{Vaccination}"
+				}],
 
-					measures : [{
-						name : 'Count',
-						value : '{Count}'} ],
+				measures: [{
+					name: 'Count',
+					value: '{Count}'
+				}],
 
-					data : {
-						path : "/Vaccine"
-					}
+				data: {
+					path: "/Vaccine"
+				}
+			});
+
+			oVizFrame.setDataset(oDataset);
+			oVizFrame.setModel(oModel);
+
+			//      4.Set Viz properties with dynamic
+			oVizFrame.setVizProperties({
+				title: {
+					text: "Vaccination Chart India"
+				},
+				plotArea: {
+					colorPalette: d3.scale.category20().range(),
+					drawingEffect: "normal"
+				}
+			});
+
+			var feedSize = new sap.viz.ui5.controls.common.feeds.FeedItem({
+				'uid': "size",
+				'type': "Measure",
+				'values': ["Count"]
+			}),
+				feedColor = new sap.viz.ui5.controls.common.feeds.FeedItem({
+					'uid': "color",
+					'type': "Dimension",
+					'values': ["Vaccination"]
 				});
-				oVizFrame.setDataset(oDataset);
-				oVizFrame.setModel(oModel);
-
-		//      4.Set Viz properties
-				oVizFrame.setVizProperties({
-					title:{
-						text : "Vaccination Chart India"
-					},
-					plotArea: {
-						colorPalette : d3.scale.category20().range(),
-						drawingEffect: "normal"
-						}});
-
-				var feedSize = new sap.viz.ui5.controls.common.feeds.FeedItem({
-					  'uid': "size",
-					  'type': "Measure",
-					  'values': ["Count"]
-					}),
-					feedColor = new sap.viz.ui5.controls.common.feeds.FeedItem({
-					  'uid': "color",
-					  'type': "Dimension",
-					  'values': ["Vaccination"]
-					});
-				oVizFrame.addFeed(feedSize);
-				oVizFrame.addFeed(feedColor);
+			oVizFrame.addFeed(feedSize);
+			oVizFrame.addFeed(feedColor);
 		},
 
 		onExit: function () {
@@ -119,6 +160,43 @@ sap.ui.define([
 				Log.info("SplitApp object can't be found");
 			}
 			return result;
+		},
+		onPress: function (oEvent) {
+			var oItem = oEvent.getSource();
+			var oBindingContext = oItem.getBindingContext();
+			var oModel = this.getView().getModel();
+			var oSettingsModel = this.getView().getModel('settings');
+			oSettingsModel.setProperty("/navigatedItem", oModel.getProperty("ProductId", oBindingContext));
+		},
+
+		isNavigated: function (sNavigatedItemId, sItemId) {
+			return sNavigatedItemId === sItemId;
+		},
+		onBtnPress : function (oEvent) {
+			function getDate(n) {
+				var date = new Date();
+				var today = new Date(date);
+				today.setDate(today.getDate() + n);
+				var dd = today.getDate();
+				var mm = today.getMonth() + 1;
+				var yyyy = today.getFullYear();
+				if (dd < 10) {
+					dd = '0' + dd;
+				}
+				if (mm < 10) {
+					mm = '0' + mm;
+				}
+				var today = dd + '-' + mm + '-' + yyyy;
+
+				return today;
+			}
+			var pincode = this.getView().byId("pincode").getValue();
+			var table = this.getView().byId("tableId");
+			console.log(pincode);
+			var sPath = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode="+pincode+"&date="+getDate(0);
+			var oPinModel = new sap.ui.model.json.JSONModel(sPath);
+			console.log(oPinModel);
+			table.setModel(oPinModel)
 		}
 
 	});
