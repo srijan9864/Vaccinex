daf0a026ed2e15062dad56991202de97beb25108
Srijan Sarmah <68153952+srijan9864@users.noreply.github.com>
2021-05-30 13:20:17 +0530

    District search complete

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -333,9 +333,137 @@ sap.ui.define([
 		handleChange: function(oEvent){
 			var oValidatedComboBox = oEvent.getSource();
 			var oSelectedKey = oValidatedComboBox.getSelectedKey();
+			var oDistrictModel = new sap.ui.model.json.JSONModel("https://cdn-api.co-vin.in/api/v2/admin/location/districts/"+oSelectedKey);
+		
+			this.getView().byId("district").setModel(oDistrictModel);
+		
+	},
+	onBtnPress1: function (oEvent) {
+	var that = this;
+			function getDate(n) {
+				var sDate = that.getView().byId("Date2").getDateValue();
+				var today = new Date(sDate);
+				today.setDate(today.getDate() + n);
+				var dd = today.getDate();
+				var mm = today.getMonth() + 1;
+				var yyyy = today.getFullYear();
+				if (dd < 10) {
+					dd = '0' + dd;
+				}
+				if (mm < 10) {
+					mm = '0' + mm;
+				}
+				var today = dd + '-' + mm + '-' + yyyy;
 
-		var oDistrictModel = new sap.ui.model.json.JSONModel("https://cdn-api.co-vin.in/api/v2/admin/location/districts/"+oSelectedKey);
-		this.getView().byId("district").setModel(oDistrictModel);
+				return today;
+			}
+			var oDistrict = this.getView().byId("district").getSelectedKey();
+			var table = this.getView().byId("table1Id");
+			var table1 = this.getView().byId("table1Id1");
+			var table2 = this.getView().byId("table1Id2");
+			var table3 = this.getView().byId("table1Id3");
+			var table4 = this.getView().byId("table1Id4");
+			var table5 = this.getView().byId("table1Id5");
+			var table6 = this.getView().byId("table1Id6");
+			var vBox = this.getView().byId("Vbox2");
+			var checboxes = this.getView().byId("checboxpanel1");
+
+			var dateModel = this.getView().getModel("dates");
+			dateModel.setDefaultBindingMode(sap.ui.model.BindingMode.OneWay);
+			var dateData = {};
+			dateData = {
+				dates: {
+					today: getDate(0),
+					today_1: getDate(1),
+					today_2: getDate(2),
+					today_3: getDate(3),
+					today_4: getDate(4),
+					today_5: getDate(5),
+					today_6: getDate(6)
+				}
+			} ;
+
+			dateModel.setData(dateData);
+			this.getView().setModel(dateModel, "dates");
+			vBox.setVisible(true);
+			checboxes.setVisible(true);
+			var modeArray = [];
+			for (var i = 0; i < 7; i++) {
+
+				var sPath = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=" + oDistrict + "&date=" + getDate(i);
+				var oPinModel = new sap.ui.model.json.JSONModel(sPath);
+				modeArray.push(oPinModel);
+				sPath = "";
+				oPinModel = undefined;
+			}
+			table.setModel(modeArray[0]);
+			table1.setModel(modeArray[1]);
+			table2.setModel(modeArray[2]);
+			table3.setModel(modeArray[3]);
+			table4.setModel(modeArray[4]);
+			table5.setModel(modeArray[5]);
+			table6.setModel(modeArray[6]);
+		},
+		onCheckbox1: function(oEvent){
+			var filtermodel = new sap.ui.model.json.JSONModel();
+			var filterArray = [];
+			var oData1 = { filter: filterArray };
+			var oFilter;
+			var oBinding = this.getView().byId("table1Id").getBinding("items");
+			var oBinding1 = this.getView().byId("table1Id1").getBinding("items");
+			var oBinding2 = this.getView().byId("table1Id2").getBinding("items");
+			var oBinding3 = this.getView().byId("table1Id3").getBinding("items");
+			var oBinding4 = this.getView().byId("table1Id4").getBinding("items");
+			var oBinding5 = this.getView().byId("table1Id5").getBinding("items");
+			var oBinding6 = this.getView().byId("table1Id6").getBinding("items");
+			oBinding.filter([]);
+			oBinding1.filter([]);
+			oBinding2.filter([]);
+			oBinding3.filter([]);
+			oBinding4.filter([]);
+			oBinding5.filter([]);
+			oBinding6.filter([]);
+			if (this.getView().byId("check118").getSelected()){
+				oFilter= new sap.ui.model.Filter("min_age_limit", "EQ", "18");
+				filterArray.push(oFilter);
+			}
+
+			if (this.getView().byId("check145").getSelected()){
+				oFilter= new sap.ui.model.Filter("min_age_limit", "EQ", "45");
+				filterArray.push(oFilter);
+			}
+
+
+			if (this.getView().byId("check1covaxin").getSelected()){
+				oFilter= new sap.ui.model.Filter("vaccine", "EQ", "COVAXIN");
+				filterArray.push(oFilter);
+			}
+
+			if (this.getView().byId("checkcovishield").getSelected()){
+				oFilter= new sap.ui.model.Filter("vaccine", "EQ", "COVISHIELD");
+				filterArray.push(oFilter);
+			}
+
+			if (this.getView().byId("check1sputnik").getSelected()){
+				oFilter= new sap.ui.model.Filter("vaccine", "EQ", "SPUTNIK V");
+				filterArray.push(oFilter);
+			}
+			if (this.getView().byId("check1free").getSelected()){
+				oFilter= new sap.ui.model.Filter("fee_type", "EQ", "Free");
+				filterArray.push(oFilter);
+			}
+
+			if (this.getView().byId("check1paid").getSelected()){
+				oFilter= new sap.ui.model.Filter("fee_type", "EQ", "Paid");
+				filterArray.push(oFilter);
+			}
+			oBinding.filter(filterArray);
+			oBinding1.filter(filterArray);
+			oBinding2.filter(filterArray);
+			oBinding3.filter(filterArray);
+			oBinding4.filter(filterArray);
+			oBinding5.filter(filterArray);
+			oBinding6.filter(filterArray);
 		}
 
 	});
